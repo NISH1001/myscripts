f28961dde658ba9dc8e2c63141f9d3bab608762e
NISH1001 <nishanpantha@gmail.com>
2021-02-26 21:45:22 +0545

    Add a CLI to search over my cached cmd logs
    Usage:
    
    `python scmd.py <arg1> <arg2> ...`
    
    The arguments are **AND**ed with each line in the logs.
    
    This can be aliased in bashrc accordingly.

---
+++ bin/scmd.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import glob
+import pydoc
+
+import os
+import sys
+import subprocess
+
+
+LOGS_DIR = os.getenv("LOGS_DIR", os.path.expanduser("~/.logs/"))
+CMD_SPLITTER = os.getenv("CMD_SPLITTER", 2)
+
+
+def main():
+    assert os.path.exists(LOGS_DIR)
+
+    args = sys.argv[1:]
+    if not args:
+        sys.exit(0)
+
+    files = glob.glob(os.path.join(LOGS_DIR, "*.log"))
+    files.sort(key=os.path.getmtime)
+    nfiles = len(files)
+    print(f"{nfiles} files")
+
+    if not nfiles:
+        sys.exit(0)
+
+    _loader = lambda x: open(x).readlines()
+    logs = map(lambda x: [l.strip() for l in open(x).readlines()], files)
+    logs = list(filter(lambda x: len(x) > 0, logs))
+    nlogs = len(logs)
+    print(f"{nlogs} non-empty logs")
+    print("-" * 10)
+
+    matches = []
+    for log in logs:
+        for line in log:
+            m = all([arg in line for arg in args])
+            if m:
+                matches.append(" ".join(line.split()[CMD_SPLITTER:]))
+
+    print(f"{len(matches)} matches")
+    res = "\n".join(matches)
+    pydoc.pager(res)
+
+
+if __name__ == "__main__":
+    main()
